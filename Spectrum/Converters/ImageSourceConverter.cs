76e8cd1d6f9764cde74e06c8eba89f6820155be5
qJake <spike@spikex.net>
2013-10-20 18:05:53 -0500

    Added a bunch of files.

---
+++ Spectrum/Converters/ImageSourceConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Spectrum
+{
+    /// <summary>
+    /// A type converter for visibility and boolean values.
+    /// </summary>
+    public class ImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                BitmapImage img = new BitmapImage();
+
+                string path = value.ToString();
+
+                img.BeginInit();
+                img.UriSource = new Uri("pack://application:,,,/" + path, UriKind.Absolute);
+                img.EndInit();
+
+                return img;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
