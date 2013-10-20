bf6c0177ce937100f262f778e79a631c654dcc76
qJake <spike@spikex.net>
2013-10-19 23:49:01 -0500

    Progress towards clicking to set color.

--- Spectrum/Controls/LightStatus.xaml.cs
+++ Spectrum/Controls/LightStatus.xaml.cs
@@ -30,6 +30,20 @@ namespace Spectrum.Controls
             light = l;
 
             DataContext = light;
+
+            HSBColor hsb;
+            switch (light.State.CurrentColorMode.ToLower())
+            {
+                case "ct":
+                    hsb = new HSBColor(MathEx.TranslateValue(light.State.ColorTemperature, 137, 500, 2000, 11500, true));
+                    break;
+                default:
+                    hsb = new HSBColor(l.State.Hue / 255, l.State.Saturation, l.State.Brightness);
+                    break;
+            }
+
+            var color = hsb.Color;
+            OnDot.Fill = new SolidColorBrush(new Color() { A = 255, R = color.R, G = color.G, B = color.B });
         }
     }
 }
--- Spectrum/Pages/Now.xaml.cs
+++ Spectrum/Pages/Now.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,9 +28,7 @@ namespace Spectrum.Pages
         public Now()
         {
             InitializeComponent();
-
             lights = new LightCollection();
-
             Refresh();
         }
 
@@ -43,6 +42,47 @@ namespace Spectrum.Pages
             {
                 LightContainer.Children.Add(new LightStatus(light));
             }
+        }
+
+        private void Saturation_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var mousePos = e.GetPosition((IInputElement)sender);
+
+            var hue = MathEx.TranslateValue((int)mousePos.X, 0, (int)((FrameworkElement)sender).ActualWidth, 0, 255);
+            var sat = 255 - MathEx.TranslateValue((int)mousePos.Y, 0, (int)((FrameworkElement)sender).ActualHeight, 0, 255);
+
+            HSBColor color = new HSBColor(hue, sat, 255);
+            BriTop.Color = new Color() { A = 255, R = color.Color.R, G = color.Color.G, B = color.Color.B };
+
+            new LightStateBuilder()
+                .For(lights[5])
+                .Hue((ushort)(color.H * 255))
+                .Saturation((byte)color.S)
+                .Brightness((byte)color.B)
+                .TurnOn()
+                .Apply();
+
+            Refresh();
+        }
+
+        private void SelectorCT_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var mousePos = e.GetPosition((IInputElement)sender);
+
+            var visualct = MathEx.TranslateValue((int)mousePos.Y, 0, (int)((FrameworkElement)sender).ActualHeight, 2000, 11500);
+            var ct = MathEx.TranslateValue((int)mousePos.Y, 0, (int)((FrameworkElement)sender).ActualHeight, 137, 500, true);
+
+            HSBColor color = new HSBColor(visualct);
+            color.B = 255;
+            BriTop.Color = new Color() { A = 255, R = color.Color.R, G = color.Color.G, B = color.Color.B };
+
+            new LightStateBuilder()
+                .For(lights[5])
+                .ColorTemperature((ushort)ct)
+                .TurnOn()
+                .Apply();
+
+            Refresh();
         }
     }
 }
